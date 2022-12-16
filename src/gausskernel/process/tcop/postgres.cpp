c52a334ad5c5e72f36d2ecf607b828d3382c85c5
johnfangAFW <jfang@futurewei.com>
2022-12-15 20:20:14 -0800

    reenable K2PgPrepareCacheRefreshIfNeeded

--- src/gausskernel/process/tcop/postgres.cpp
+++ src/gausskernel/process/tcop/postgres.cpp
@@ -11630,22 +11630,23 @@ static void K2PgPrepareCacheRefreshIfNeeded(MemoryContext oldcontext,
 
 	*need_retry = false;
 
-    return;
 	/*
 	 * A retry is only required if the transaction is handled by K2PG.
 	 */
 	if (!IsK2PgEnabled() || t_thrd.log_cxt.errordata_stack_depth < 0)
 		return;
 
+    elog(INFO, "K2PgPrepareCacheRefreshIfNeeded is called");
+
 	/* Get error data */
 	ErrorData *edata = NULL;
 	MemoryContextSwitchTo(oldcontext);
 	edata = CopyErrorData();
 
-        if(edata != NULL) {
-            elog(INFO, "ErrorData: file: %s, func: %s, line: %d, message: %s, detail: %s\n", edata->filename, edata->funcname,
-                        edata->lineno, edata->message ? edata->message : "", edata->detail ? edata->detail : "");
-        }
+        // if(edata != NULL) {
+        //     elog(INFO, "ErrorData: file: %s, func: %s, line: %d, message: %s, detail: %s\n", edata->filename, edata->funcname,
+        //                 edata->lineno, edata->message ? edata->message : "", edata->detail ? edata->detail : "");
+        // }
 
 	bool is_retryable_err = K2PgNeedRetryAfterCacheRefresh(edata);
 	if ((table_to_refresh = strstr(edata->message,
